b76f12a11024d8c1de483b0638fb5aff97b3701d
EmilianoCerda <emil.cerda@duocuc.cl>
2026-04-26 18:30:07 -0400

    Juego service (falta validaciones todavia), Webclient creado

--- src/main/java/com/volvo/volvo/Configuration/WebClientConfig.java
+++ src/main/java/com/volvo/volvo/Configuration/WebClientConfig.java
@@ -1,8 +1,33 @@
 package com.volvo.volvo.Configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import lombok.Value;
 
 @Configuration
 public class WebClientConfig {
+    @Value("${services.genero.url}")
+    private String generoServiceUrl;
+
+    @Value("${services.plataforma.url}")
+    private String plataformaServiceUrl;
+
+    @Value("${services.estudio.url}")
+    private String estudioServiceUrl;
+
+    @Bean
+    public WebClient generowebClient() {
+        return WebClient.builder().baseUrl(generoServiceUrl).build();
+    }
+    @Bean
+    public WebClient plataformawebClient() {
+        return WebClient.builder().baseUrl(plataformaServiceUrl).build();
+    }
+    @Bean
+    public WebClient estudiowebClient() {
+        return WebClient.builder().baseUrl(estudioServiceUrl).build();
+    }
 
 }
--- src/main/java/com/volvo/volvo/JuegosService/JuegoService.java
+++ src/main/java/com/volvo/volvo/JuegosService/JuegoService.java
@@ -1,5 +1,120 @@
 package com.volvo.volvo.JuegosService;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.volvo.volvo.DTO.EstudioDTO;
+import com.volvo.volvo.DTO.GeneroDTO;
+import com.volvo.volvo.DTO.JuegoPedidoDTO;
+import com.volvo.volvo.DTO.JuegoRespuestaDTO;
+import com.volvo.volvo.DTO.PlataformaDTO;
+import com.volvo.volvo.JuegosRepository.JuegoRepository;
+import com.volvo.volvo.Model.Juego;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class JuegoService {
+    @Autowired
+    private JuegoRepository repositorio;
+
+
+    //------son los Beans creados en webclient//
+    private final WebClient generowebClient;
+    private final WebClient plataformawebClient;
+    private final WebClient estudiowebClient;
+
+//-----metodo para listar todossss-------//
+    public List<JuegoRespuestaDTO> obtenerJuegos(){
+        return repositorio.findAll()
+            .stream()
+            .map(this::enriquecerJuego)
+            .collect(Collectors.toList());
+    }
+//---------Obtener por id usando El DTO--------//
+    public JuegoRespuestaDTO obtenerJuegoporId(Long id){
+        Juego juego = repositorio.findById(id)
+                    .orElseThrow(()-> new RuntimeException("Juego no encontrado con id;"+id));
+        return enriquecerJuego(juego);            
+    }
+
+    ///--------CREAR JUego-------//
+
+    public JuegoRespuestaDTO crearJuego(JuegoPedidoDTO pedido){
+
+        //validamos que los id existen en sus respectivos servicios
+        validarIdExterno(pedido.getGeneroid(), pedido.getPlataformaid(), pedido.getEstudioid());
+
+        Juego juego = new Juego();
+        juego.setTitulo(pedido.getTitulo());
+        juego.setDescripcion(pedido.getDescripcion());
+        juego.setAnioLanzamiento(pedido.getAnioLanzamiento());
+        juego.setGeneroid(pedido.getGeneroid());
+        juego.setPlataformaid(pedido.getPlataformaid());
+        juego.setEstudioid(pedido.getEstudioid());
+
+        Juego guardado = repositorio.save(juego);
+        return enriquecerJuego(guardado);
+    }
+
+    //-----Metodo exclusivo: metodo para enriquecer un Juego con los datos de otros
+    //servicios segun Webclient. recibe Entidad Juego que solo tiene los ids 
+    //para devolver un JuegoRespuestaDTO que tiene los objetos completos
+    private JuegoRespuestaDTO enriquecerJuego(Juego juego){
+
+        //Llama al genero sercicio con un Get localhost:8081/api/v1/...
+        GeneroDTO genero = generowebClient.get()
+        .uri("/api/v1/generos/{id}",juego.getGeneroid())
+        .retrieve()
+        .onStatus(status->status.is4xxClientError(),
+         response-> response.bodyToMono(String.class)
+        .map(body -> new RuntimeException("Genero con ID"+ juego.getGeneroid()+"no encontrado en genero Servicio"
+    ))
+)
+    .bodyToMono(GeneroDTO.class).block();
+
+
+    PlataformaDTO platforma = plataformawebClient.get()
+                .uri("/api/v1/platforma/{id}", juego.getPlataformaid())
+                .retrieve()
+                .onStatus(
+                    status -> status.is4xxClientError(),
+                    response -> response.bodyToMono(String.class)
+                        .map(body -> new RuntimeException(
+                            "Plataforma con ID " + juego.getPlataformaid() + " no encontrada en Platforma Service"
+                        ))
+                )
+                .bodyToMono(PlataformaDTO.class)
+                .block();
+
+    EstudioDTO studio = estudiowebClient.get()
+                .uri("/api/studios/{id}", juego.getEstudioid())
+                .retrieve()
+                .onStatus(
+                    status -> status.is4xxClientError(),
+                    response -> response.bodyToMono(String.class)
+                        .map(body -> new RuntimeException(
+                            "Estudio con ID " + juego.getEstudioid() + " no encontrado en Estudio Service"
+                        ))
+                )
+                .bodyToMono(EstudioDTO.class)
+                .block();
+
+    JuegoRespuestaDTO respuesta = new JuegoRespuestaDTO();
+        respuesta.setId(juego.getId());
+        respuesta.setTitulo(juego.getTitulo());
+        respuesta.setDescipcion(juego.getDescripcion());
+        respuesta.setAnioLanzamiento(juego.getAnioLanzamiento());
+        respuesta.setGenero(genero);
+        respuesta.setPlataforma(plataforma);
+        respuesta.setEstudio(estudio);
+
+        return respuesta;
+    }
 
 }
